4cfe442a778fcf8666b9b862c8955efa07ad30bb
Jason Cheng <278211526@qq.com>
2017-04-26 15:40:42 +0800

    Add files via upload

---
+++ config/env/development.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  db: {
+    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/welfare',
+    //uri: 'mongodb://LINDA4-W7:27017/welfare',
+    //uri: 'mongodb://LUJE6-W7:27017/welfare',
+    options: {
+      user: '',
+      pass: ''
+    },
+    // Enable mongoose debug mode
+    debug: process.env.MONGODB_DEBUG || false
+  },
+  log: {
+    // logging with Morgan - https://github.com/expressjs/morgan
+    // Can specify one of 'combined', 'common', 'dev', 'short', 'tiny'
+    format: 'dev',
+    fileLogger: {
+      directoryPath: process.cwd(),
+      fileName: 'app.log',
+      maxsize: 10485760,
+      maxFiles: 2,
+      json: false
+    }
+  }
+};
